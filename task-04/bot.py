930e5aaae2dcd1a5147a1a94b355ab57c6daff00
Hemanth <hemanthmano018@gmail.com>
2023-03-02 19:29:44 +0530

    Updated task4

--- task-04/bot.py
+++ task-04/bot.py
@@ -3,11 +3,12 @@ import telebot
 import requests
 import csv
 
-# TODO: 1.1 Get your environment variables 
+# TODO: 1.1 Get your environment variables
 yourkey = os.getenv("API_KEY")
 bot_id = os.getenv("BOT_ID")
 
 bot = telebot.TeleBot(bot_id)
+
 
 @bot.message_handler(commands=['start', 'hello'])
 def greet(message):
@@ -15,13 +16,13 @@ def greet(message):
     botRunning = True
     bot.reply_to(
         message, 'Hello there! I am a bot that will show movie information for you and export it in a CSV file.\nSee /help to see the commands.')
-    
+
+
 @bot.message_handler(commands=['stop', 'bye'])
 def goodbye(message):
     global botRunning
     botRunning = False
     bot.reply_to(message, 'Bye!\nHave a good time')
-    
 
 
 @bot.message_handler(func=lambda message: botRunning, commands=['help'])
@@ -35,15 +36,17 @@ def getMovie(message):
     movie_name = message.text[7:]
     if movie_name:
         # TODO: 1.2 Get movie information from the API
-        movie_details = requests.get("http://www.omdbapi.com/?apikey="+yourkey+"&t="+movie_name).json()
+        movie_details = requests.get(
+            "http://www.omdbapi.com/?apikey="+yourkey+"&t="+movie_name).json()
         title = movie_details['Title']
         year = movie_details['Year']
         rating = movie_details['imdbRating']
-        
+        photo_link = movie_details['Poster']
+
         # TODO: 1.3 Show the movie information in the chat window
-        bot.reply_to(message, 
-        "Title: "+ title + "\nYear of Release: "+ year + "\nIMDB Rating: " + rating)
-        
+        bot.send_photo(message.chat.id, photo_link,
+                       "Title: " + title + "\nYear of Release: " + year + "\nIMDB Rating: " + rating)
+
         # TODO: 2.1 Create a CSV file and dump the movie information in it
         with open("Movie details.csv", 'a') as file:
             heading = ["Title", "Year of Release", "IMDB Rating"]
@@ -56,16 +59,17 @@ def getMovie(message):
     else:
         bot.reply_to(message, 'I did not understand '+'\N{confused face}')
 
-  
+
 @bot.message_handler(func=lambda message: botRunning, commands=['export'])
 def getList(message):
     bot.reply_to(message, 'Generating file...')
-    #TODO: 2.2 Send downlodable CSV file to telegram chat
+    # TODO: 2.2 Send downlodable CSV file to telegram chat
     bot.send_document(message.chat.id, open("Movie details.csv"))
 
 
 @bot.message_handler(func=lambda message: botRunning)
 def default(message):
     bot.reply_to(message, 'I did not understand '+'\N{confused face}')
-    
+
+
 bot.infinity_polling()
